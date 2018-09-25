8b33f3421cb5f7b92f75fe01a0c3233b36aa4856
Marc Thomas <marcthomas.dev@gmail.com>
2018-09-25 18:29:02 +0200

    Extension configuration : add a new option 'center' (without replacing the calendar this time) in 'Positioning' select.

--- extension/prefs.js
+++ extension/prefs.js
@@ -60,6 +60,7 @@ class HamsterSettingsWidget extends Gtk.Grid {
         placementOptions.set(placementOptions.append(), [0, 1], ["Default", 0]);
         placementOptions.set(placementOptions.append(), [0, 1], ["Replace calendar", 1]);
         placementOptions.set(placementOptions.append(), [0, 1], ["Replace activities", 2]);
+        placementOptions.set(placementOptions.append(), [0, 1], ["Center", 3]);
 
         let placementCombo = new Gtk.ComboBox({
             model: placementOptions,
